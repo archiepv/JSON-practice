76d5b29f37d9cc32bd5466942b2796dc2055875f
archiepv <archiepv@uw.edu>
2018-07-26 20:55:43 -0700

    created a validation function forthe forms

--- script.js
+++ script.js
@@ -1,51 +1,104 @@
 /*---JAVASCRIPT DOCUMENT---*/
 let house = {
-	exterior: "brick walls",
-	roof: "solar panels",
-	bedrooms: 3,
-	bathrooms: 2,
-	sqft: 1800,
-	balcony: true
+  exterior: "wood siding",
+  roof: "spanish tile",
+  bedrooms: 2,
+  bathrooms: 2,
+  sqft: 1200,
+  hasYard: true
 }
 
 let neighborhood = [
-	house,
-	{
-		exterior: "wood",
-		roof: "modern tiles",
-		bedrooms: 5,
-		bathrooms: 3,
-		sqft: 1900,
-		balcony: true
-	},
-	{
-		exterior: "slate",
-		roof: "straws",
-		bedrooms: 1,
-		bathrooms: 1,
-		sqft: 600,
-		balcony: true
-	}
+  house
 ]
 
-for(let i=0; i < neighborhood.length; i++) {
-	const body = document.querySelector("body")
-	build(neighborhood[i], body)
-	let divider = document.createElement("p")
-	divider.innerHTML = "--------------"
-	body.appendChild(divider)
-	
+function drawNeighborhood(){
+  let container = document.querySelector("div#container")
+  container.innerHTML = ""
+  
+  for(let i=0; i<neighborhood.length;i++){
+    build(neighborhood[i], container)
+    let divider = document.createElement("p")
+    divider.innerHTML = "🌲🌲🌲🌲🌲🌲🌲🌲🌲🌲"
+    container.appendChild(divider)
+  }
 }
 
-function build(structure, body) {
-	for (let key in house){
-		//console.log("Key: ", key, " value: ", house[key])//
+function build(obj, container){
+  let div = document.createElement("div")
+  div.classList.add("house")
 
-		//---create a nice paragraph---//
-		let p = document.createElement("p")
-		p.innerHTML = key + ": " + structure[key]
+  for (let key in obj){
+    // create a nice paragraph
+    let p = document.createElement("p")
+    p.innerHTML = key + ": " + obj[key]
 
-		//---append it to the body---//
-		body.appendChild(p);	
-	}
+    // append it to the "house" div
+    div.appendChild(p)
+  }
+  // append "house" div to container
+  container.appendChild(div)
 }
+
+// New form submission handler
+const form = document.querySelector("form")
+
+form.addEventListener("submit", function(event){
+  event.preventDefault()
+
+  let errorMessages = ""
+
+  let newRoof = this.roof.value
+  if(newRoof === ""){
+    newRoof = "unknown"
+  }
+  let newExterior = this.exterior.value
+  if(newExterior === ""){
+    newExterior = "uknown"
+  }
+
+  let newBathrooms = parseFloat(this.bathrooms.value)
+  if(isNaN(newBathrooms) || newBathrooms > 5 || newBathrooms < 1){
+    errorMessages = errorMessages + "Number of bathrooms must be a number between 1 and 5"
+  }
+
+  let newBedrooms = parseInt(this.bedrooms.value)
+  if(isNaN(newBedrooms) || newBedrooms > 5 || newBedrooms < 1){
+    errorMessages = errorMessages + " Number of bedrooms must be a number between 1 and 5"
+  }
+
+  let newSqft = parseInt(this.sqft.value)
+  if(isNaN(newSqft)){
+    errorMessages = errorMessages + " Square Footage is required"
+  }
+
+  let newHasYard = this.hasYard.value
+  if(newHasYard === ""){
+    errorMessages = errorMessages + " Has Yard is required"
+  } else {
+    newHasYard = newHasYard === "true"
+  }
+
+  if(errorMessages !== ""){
+    // print to page, return
+    // TODO: Print at top of form, color red
+    let p = document.createElement("p")
+    p.innerHTML = errorMessages
+    this.appendChild(p)
+    return
+  } else {
+    // TODO: clear form fields
+    // create object, put in neighborhood
+    const newHouse = {
+      exterior: newExterior,
+      roof: newRoof,
+      bedrooms: newBedrooms,
+      bathrooms: newBathrooms,
+      sqft: newSqft,
+      hasYard: newHasYard
+    }
+    neighborhood.push(newHouse)
+    drawNeighborhood()
+  }
+
+})
